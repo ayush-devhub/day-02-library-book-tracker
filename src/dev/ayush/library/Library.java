a53c1fbf6829335027f53f72b6969ff0ef8e1dba
ayush-devhub <ayush.devhub@gmail.com>
2025-08-29 23:29:44 +0530

    feat: add menu driven CLI in LibraryApp

--- src/dev/ayush/library/Library.java
+++ src/dev/ayush/library/Library.java
@@ -1,12 +1,13 @@
 package dev.ayush.library;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
     private List<Book> books;
 
-    public Library(List<Book> books) {
-        this.books = books;
+    public Library() {
+        books = new ArrayList<>();
     }
 
     public void addBook(Book book){
@@ -32,18 +33,26 @@ public class Library {
     }
 
     public void listAvailableBooks(){
-        for (Book book : books) {
-            if (book.isAvailable()){
-                System.out.println(book);
+        if (!books.isEmpty()){
+            for (Book book : books) {
+                if (book.isAvailable()){
+                    System.out.println(book);
+                }
             }
+        }else {
+            System.out.println("No books available!");
         }
     }
 
     public void listCheckedOutBooks(){
-        for (Book book : books) {
-            if (!book.isAvailable()){
-                System.out.println(book);
+        if (!books.isEmpty()){
+            for (Book book : books) {
+                if (!book.isAvailable()){
+                    System.out.println(book);
+                }
             }
+        }else {
+            System.out.println("No books available in the library.");
         }
     }
 }
--- src/dev/ayush/library/LibraryApp.java
+++ src/dev/ayush/library/LibraryApp.java
@@ -1,8 +1,75 @@
 package dev.ayush.library;
 
+import java.util.Scanner;
+
 public class LibraryApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
 
+        while (true) {
+            showMenu();
+            System.out.print("Enter choice: ");
+            int choice = scanner.nextInt();
+            System.out.println();
+            handleUserInput(choice, library, scanner);
+            System.out.println();
+        }
     }
 
+    public static void showMenu() {
+        System.out.println("===== Library Book Tracker =====\n" +
+                "1. Add Book\n" +
+                "2. Checkout Book\n" +
+                "3. Return Book\n" +
+                "4. List Available Books\n" +
+                "5. List Checked Out Books\n" +
+                "6. Exit");
+    }
+
+
+    public static void handleUserInput(int choice, Library library, Scanner scanner) {
+        scanner.nextLine();
+        switch (choice){
+            case 1:{
+                String isbn, author, title;
+                System.out.print("Enter Title: ");
+                title = scanner.nextLine();
+                System.out.print("Enter Author: ");
+                author = scanner.nextLine();
+                System.out.print("Enter ISBN: ");
+                isbn = scanner.nextLine();
+                library.addBook(new Book(isbn, author, title));
+                System.out.println("Book added successfully!");
+                break;
+            }
+            case 2:{
+                System.out.print("Enter isbn: ");
+                String isbn = scanner.nextLine();
+                library.checkoutBook(isbn);
+                break;
+            }
+
+            case 3:{
+                System.out.print("Enter isbn: ");
+                String isbn = scanner.nextLine();
+                library.returnBook(isbn);
+                break;
+            }
+
+            case 4:{
+                library.listAvailableBooks();
+                break;
+            }
+
+            case 5:{
+                library.listCheckedOutBooks();
+                break;
+            }
+
+            case 6:{
+                System.exit(0);
+            }
+        }
+    }
 }
