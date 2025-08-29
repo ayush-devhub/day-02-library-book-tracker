84f82c63708177032dd906c105c8436666963ca4
ayush-devhub <ayush.devhub@gmail.com>
2025-08-29 22:55:51 +0530

    feat: add Book model with availability flag

--- src/dev/ayush/library/Book.java
+++ src/dev/ayush/library/Book.java
@@ -1,4 +1,32 @@
 package dev.ayush.library;
 
 public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(String isbn, String author, String title) {
+        this.isbn = isbn;
+        this.author = author;
+        this.title = title;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available){
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
--- src/dev/ayush/library/LibraryApp.java
+++ src/dev/ayush/library/LibraryApp.java
@@ -1,4 +1,8 @@
 package dev.ayush.library;
 
 public class LibraryApp {
+    public static void main(String[] args) {
+
+    }
+
 }
